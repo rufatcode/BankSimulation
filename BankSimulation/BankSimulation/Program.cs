c11b1466f3f4aca87ed0aa9f9c0af9b780018f9c
Rufat <rufat@Rufats-MacBook-Air.local>
2023-06-08 00:57:59 +0400

    feat:add my first part of my End Project

---
+++ BankSimulation/BankSimulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
